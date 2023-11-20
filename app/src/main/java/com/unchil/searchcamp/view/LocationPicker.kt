6b16544baf2ba263f9b41ea368dc54871b828b2a
unchil <yeopo77@icloud.com>
2023-11-20 23:02:49 +0900

    Merge remote-tracking branch 'origin/main'

--- app/src/main/java/com/unchil/searchcamp/view/LocationPicker.kt
+++ app/src/main/java/com/unchil/searchcamp/view/LocationPicker.kt
@@ -16,12 +16,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ShapeDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,84 +33,12 @@ import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.util.lerp
 import androidx.wear.compose.material.Text
+import com.unchil.searchcamp.LocalUsableHaptic
 import com.unchil.searchcamp.data.VWorldService
 import com.unchil.searchcamp.db.entity.SiDo_TBL
 import com.unchil.searchcamp.db.entity.SiGunGu_TBL
+import kotlinx.coroutines.launch
 import kotlin.math.absoluteValue
-
-/*
-@Composable
-fun AdministrativeDistrictSiDoPicker(
-    dataList:List<SiDo_TBL>,
-    onSelectedHandler:(VWorldService, String, String)-> Unit,
-) {
-
-    val hapticFeedback = LocalHapticFeedback.current
-
-    val configuration = LocalConfiguration.current
-
-    val sido_state = rememberPickerState(
-        initialNumberOfOptions =dataList.size,
-        initiallySelectedOption= 0,
-        repeatItems = false)
-
-
-
-    LaunchedEffect (key1 = configuration.orientation) {
-        sido_state.scrollToOption(0)
-    }
-
-
-
-    LaunchedEffect(key1 =sido_state.isScrollInProgress) {
-        if (sido_state.isScrollInProgress) {
-    //        hapticFeedback.performHapticFeedback(HapticFeedbackType.TextHandleMove)
-        }else {
-
-            val  administrativeDistrictSiDo:Pair<String, String>  = if(dataList.size <= sido_state.selectedOption){
-                Pair(  dataList.last().ctprvn_cd, dataList.last().ctp_kor_nm)
-            }else {
-                Pair(dataList[sido_state.selectedOption].ctprvn_cd , dataList[sido_state.selectedOption].ctp_kor_nm )
-            }
-
-            onSelectedHandler(
-                VWorldService.LT_C_ADSIDO_INFO,
-                administrativeDistrictSiDo.first,
-                administrativeDistrictSiDo.second
-            )
-        }
-    }
-
-    Picker(
-        modifier = Modifier
-            .clip(shape = ShapeDefaults.ExtraSmall)
-            .size(width = 160.dp, height = 100.dp),
-        state = sido_state,
-        contentDescription = null,
-        gradientColor =  MaterialTheme.colorScheme.onPrimaryContainer.copy(alpha = 0.6f),
-    ) {
-
-        val  text  = if(dataList.size <= it){
-            dataList.last().ctp_kor_nm
-        }else {
-            dataList[it].ctp_kor_nm
-        }
-
-
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            textAlign = TextAlign.Center,
-            text = text  ,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black,
-            style = MaterialTheme.typography.bodyLarge,
-        )
-
-    }
-
-}
-
- */
 
 
 @OptIn(ExperimentalFoundationApi::class)
@@ -121,6 +53,23 @@ fun AdministrativeDistrictSiDoPicker(
         initialPageOffsetFraction = 0f,
         pageCount = {  dataList.size } )
 
+    LaunchedEffect(key1 = dataList ){
+        pagerState.scrollToPage(0)
+    }
+
+    val isUsableHaptic = LocalUsableHaptic.current
+    val hapticFeedback = LocalHapticFeedback.current
+    val coroutineScope = rememberCoroutineScope()
+
+    fun hapticProcessing(){
+        if(isUsableHaptic){
+            coroutineScope.launch {
+                hapticFeedback.performHapticFeedback(HapticFeedbackType.TextHandleMove)
+            }
+        }
+    }
+
+
     LaunchedEffect(key1 = pagerState.isScrollInProgress){
         if (!pagerState.isScrollInProgress){
             onSelectedHandler(
@@ -128,6 +77,9 @@ fun AdministrativeDistrictSiDoPicker(
                 dataList[pagerState.currentPage].ctprvn_cd,
                 dataList[pagerState.currentPage].ctp_kor_nm
             )
+
+        }else {
+            hapticProcessing()
         }
     }
 
@@ -146,18 +98,15 @@ fun AdministrativeDistrictSiDoPicker(
         }
     }
 
-    val colorStops = arrayOf(
-        0.3f to Color.White,
-        0.5f to Color.LightGray,
-        1f to Color.Gray.copy(alpha = 0.3f)
-    )
+    val listColors = listOf(Color.Gray, Color.White, Color.Gray)
 
     Box(
         modifier = Modifier
             .clip(ShapeDefaults.Small)
             .width(boxWidth)
             .height(boxHeight)
-            .background(Brush.radialGradient(colorStops = colorStops)),
+            .background( Brush.verticalGradient( listColors  ))
+        ,
         contentAlignment = Alignment.Center
     ){
 
@@ -191,13 +140,13 @@ fun AdministrativeDistrictSiDoPicker(
                         )
 
                         scaleX = lerp(
-                            start = 0.7f,
+                            start = 0.8f,
                             stop = 1f,
                             fraction = 1f - pageOffset.coerceIn(0f, 1f)
                         )
 
                         scaleY = lerp(
-                            start = 0.7f,
+                            start = 0.8f,
                             stop = 1f,
                             fraction = 1f - pageOffset.coerceIn(0f, 1f)
                         )
@@ -221,6 +170,7 @@ fun AdministrativeDistrictSiDoPicker(
                     textAlign = TextAlign.Center,
                     style = MaterialTheme.typography.bodyLarge,
                 )
+
             }
 
         }
@@ -247,8 +197,22 @@ fun AdministrativeDistrictSiGunGuPicker(
         pageCount = {  dataList.size } )
 
 
+    // dataList 변경시 pagerState.currentPage 가 initial 되지 않음.
     LaunchedEffect(key1 = dataList ){
         pagerState.scrollToPage(0)
+    }
+
+
+    val isUsableHaptic = LocalUsableHaptic.current
+    val hapticFeedback = LocalHapticFeedback.current
+    val coroutineScope = rememberCoroutineScope()
+
+    fun hapticProcessing(){
+        if(isUsableHaptic){
+            coroutineScope.launch {
+                hapticFeedback.performHapticFeedback(HapticFeedbackType.TextHandleMove)
+            }
+        }
     }
 
 
@@ -259,6 +223,8 @@ fun AdministrativeDistrictSiGunGuPicker(
                 dataList[pagerState.currentPage].sig_cd,
                 dataList[pagerState.currentPage].sig_kor_nm
             )
+        } else {
+            hapticProcessing()
         }
     }
 
@@ -276,18 +242,17 @@ fun AdministrativeDistrictSiGunGuPicker(
         }
     }
 
-    val colorStops = arrayOf(
-        0.3f to Color.White,
-        0.5f to Color.LightGray,
-        1f to Color.Gray.copy(alpha = 0.3f)
-    )
+
+
+    val listColors = listOf(Color.Gray, Color.White, Color.Gray)
 
     Box(
         modifier = Modifier
             .clip(ShapeDefaults.Small)
             .width(boxWidth)
             .height(boxHeight)
-            .background(Brush.radialGradient(colorStops = colorStops)),
+            .background( Brush.verticalGradient( listColors  ))
+        ,
         contentAlignment = Alignment.Center
     ){
 
@@ -321,13 +286,13 @@ fun AdministrativeDistrictSiGunGuPicker(
                         )
 
                         scaleX = lerp(
-                            start = 0.7f,
+                            start = 0.8f,
                             stop = 1f,
                             fraction = 1f - pageOffset.coerceIn(0f, 1f)
                         )
 
                         scaleY = lerp(
-                            start = 0.7f,
+                            start = 0.8f,
                             stop = 1f,
                             fraction = 1f - pageOffset.coerceIn(0f, 1f)
                         )
@@ -360,82 +325,6 @@ fun AdministrativeDistrictSiGunGuPicker(
 
 
 }
-
-/*
-
-@Composable
-fun AdministrativeDistrictSiGunGuPicker(
-    dataList:List<SiGunGu_TBL>,
-    onSelectedHandler:(VWorldService, String, String)-> Unit
-) {
-
-    val hapticFeedback = LocalHapticFeedback.current
-    val configuration = LocalConfiguration.current
-
-    val siggState = rememberPickerState(
-        initialNumberOfOptions =dataList.size,
-        initiallySelectedOption= 0,
-        repeatItems = false)
-
-    LaunchedEffect (key1 = configuration.orientation) {
-        siggState.scrollToOption(0)
-    }
-
-
-    LaunchedEffect(key1 =siggState.isScrollInProgress) {
-        if (siggState.isScrollInProgress) {
-       //     hapticFeedback.performHapticFeedback(HapticFeedbackType.TextHandleMove)
-        } else {
-
-
-            val  administrativeDistrictSiGunGu:Pair<String, String>  = if(dataList.size <= siggState.selectedOption){
-                Pair(  dataList.last().sig_cd, dataList.last().sig_kor_nm)
-            }else {
-                Pair(dataList[siggState.selectedOption].sig_cd , dataList[siggState.selectedOption].sig_kor_nm )
-            }
-
-
-
-            onSelectedHandler(
-                VWorldService.LT_C_ADSIGG_INFO,
-                administrativeDistrictSiGunGu.first,
-                administrativeDistrictSiGunGu.second
-            )
-        }
-    }
-
-    Picker(
-        modifier = Modifier
-            .clip(shape = ShapeDefaults.ExtraSmall)
-            .size(width = 160.dp, height = 100.dp),
-        state = siggState,
-        contentDescription = null,
-        gradientColor = MaterialTheme.colorScheme.onPrimaryContainer.copy(alpha = 0.6f),
-    ) {
-
-        // 빠른 recomposable 에 의 한 dataList  size  와  initialNumberOfOptions 의 불일치 로 인한 outofbound index
-        val  text  = if(dataList.size <= it){
-            dataList.last().sig_kor_nm
-        }else {
-            dataList[it].sig_kor_nm
-        }
-
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            textAlign = TextAlign.Center,
-            text = text  ,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black,
-            style = MaterialTheme.typography.bodyLarge,
-        )
-
-
-    }
-
-}
-
-
- */
 
 
 
